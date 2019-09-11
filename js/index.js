5c3e08cf4e993c8d90626f63e9906ca088ea0546
anderson gil <andersongil.dev@gmail.com>
2019-09-10 21:20:06 -0400

    IMP:letras del hEro ubicadas en el luga correcto, y corriguiendo clases para implementacion de BEM

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* Window.onload(function(){
+console.log('Cargo el documento a los ${document} segundos')
+}); */
+
+/* $(window).on("load", function() {
+
+    // Executes when complete page is fully loaded, including
+    // all frames, objects and images
+    alert("Window is loaded");
+}); */
+
+$(Document).ready(function(){
+    
+})
