6c1e991712ec8b0dba3f8505f412396fed5b1445
anderson gil <andersongil.dev@gmail.com>
2020-07-02 18:35:37 -0400

    WIP:guardando colores a la paleta y eliminando contnido default

--- app/portafolio/src/App.js
+++ app/portafolio/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './shared/Header';
@@ -7,21 +6,7 @@ import Header from './shared/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Header/>
-      </header>
     </div>
   );
 }
