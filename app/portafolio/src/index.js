55e022b32a5e79e34e5847a16db53a0acf3eb4ee
anderson gil <andersongil.dev@gmail.com>
2020-07-01 20:21:42 -0400

    WIP: cambios menores en nombres de componentes

--- app/portafolio/src/index.js
+++ app/portafolio/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Portafolio from './App';
+import './shared/styles/bodyStyles.scss'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Portafolio />
   </React.StrictMode>,
   document.getElementById('root')
 );
