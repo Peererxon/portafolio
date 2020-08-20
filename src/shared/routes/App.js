86af4de3419e138ad5645b3598c2e67a1387d546
anderson gil <andersongil.dev@gmail.com>
2020-08-20 10:30:14 -0400

    IMP: configurando las rutas para produccion

---
+++ src/shared/routes/App.js
@@ -0,0 +1,44 @@
+import React, {Suspense,useEffect } from 'react';
+import {HashRouter,Switch,Route} from 'react-router-dom'
+import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from "materialize-css/dist/js/materialize.min.js";
+
+//import M from 'materialize-css';
+import NotFound from '../../notfound/NotFount';
+import Preloader from '../components/Preloader_line';
+//import Header from '../Header';
+import Layout from '../Layout';
+//import Home from '../../home/Home';
+//Lazy components
+const Header = React.lazy( () => import ('../Header') );
+const Home = React.lazy( () => import ('../../home/Home') ); //usando code splitting con rutas
+
+//EndLazy components
+
+function App() {
+
+  useEffect(() => {
+    var elem = document.querySelector(".sidenav"); 
+    var instance = M.Sidenav.init(elem, {
+        Edge: "left",
+        inDuration: 250
+    });
+  },[]);
+  return (
+    <HashRouter basename ="/" >
+      <Layout>
+        <Suspense fallback={<Preloader/>}>
+          <Switch>
+            <Route exact path= "/" component = {Home} />
+            <Route exact path="*" component = {NotFound}/>
+          </Switch>
+        </Suspense>
+      </Layout>
+    
+    </HashRouter>
+  );
+}
+
+export default App;
