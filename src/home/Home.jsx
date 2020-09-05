fcec14f9a9b5d7de3bb6118e861f30173e136e4f
anderson gil <andersongil.dev@gmail.com>
2020-09-04 23:47:16 -0400

    WIP: uso de libreria para animar componentes cuando entren en el viewport

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Hero from './Hero';
+import { Fade } from "react-awesome-reveal";
 
 const Home = ()=> (
-    <Hero />
+    <Fade direction="top-left">
+        <Hero />
+    </Fade>
 )
 
 export default Home;
