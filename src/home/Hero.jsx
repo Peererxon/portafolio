7d58f6d24272b8f598c6ba306612630b493fa367
anderson gil <andersongil.dev@gmail.com>
2020-09-04 23:46:33 -0400

    WIP: hero, usando svg para el hero y agregando gradiente

--- src/home/Hero.jsx
+++ src/home/Hero.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './styles/hero.scss';
+import background from '../shared/src/hero_test.svg';
 
 const Hero = () => {
     return(
         <div>
             <section>
             <div className="wrap-hero">
-                    <div className="backgraundHero">
-                        <div className="backgraundHero__h2">
-                            <h2>Hola, soy Anderson Gil</h2>
-                        </div>
+                    <div className="backgraundHero" style= {{background: `linear-gradient(141deg, rgb(17 19 23 / 67%) 10%, rgb(69 72 78 / 59%) 51%, rgb(17 19 23 / 88%) 75%),url(${background}) no-repeat center`}}>
                         <div className="backgraundHero__p">
-                            <p>Desarrollador web</p>
+                            <p>Hola, soy Anderson Gil</p>
+                        </div>
+
+                        <div className="backgraundHero__h2">
+                            <h2>Desarrollador web</h2>
                         </div>
                     </div>
                 </div>{/* <!--EndHero--> */}
