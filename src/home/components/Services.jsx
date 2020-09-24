10bbad155e9eaa1f20d5772fad1f8f7e8f83ad92
anderson gil <andersongil.dev@gmail.com>
2020-09-24 00:21:46 -0400

    WIP: instando bit y probando componente slider

---
+++ src/home/components/Services.jsx
@@ -0,0 +1,115 @@
+/* import React from 'react'
+import '../styles/services.scss';
+
+function Skills(){
+    return(
+        <div className="cardCenter">
+            <p>sd</p>
+        </div>
+    )
+}
+
+export default Skills; */
+
+import React, { Component } from 'react';
+import Slider from '@bit/akiran.react-slick.slider';
+
+export default class AsNavFor extends Component {
+  constructor(props) {
+      super(props)
+    this.state = {
+      nav1: null,
+      nav2: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+				<link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+				<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+				<style>{cssstyle}</style>
+        <h2>Slider Syncing (AsNavFor)</h2>
+        <h4>First Slider</h4>
+        <Slider
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}
+        >
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+        <h4>Second Slider</h4>
+        <Slider
+          asNavFor={this.state.nav1}
+          ref={slider => (this.slider2 = slider)}
+          slidesToShow={3}
+          swipeToSlide={true}
+          focusOnSelect={true}
+        >
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
+
+const cssstyle = `
+.container {
+  margin: 0 auto;
+  padding: 0px 40px 40px 40px;
+  width: 400px;
+}
+h3 {
+    background: #5f9ea0;
+    color: #fff;
+    font-size: 36px;
+    line-height: 100px;
+    margin: 10px;
+    padding: 2%;
+    position: relative;
+    text-align: center;
+}
+.slick-next:before, .slick-prev:before {
+    color: #000;
+}
+`
