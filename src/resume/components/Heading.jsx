75da2b80c97702b8abcc233f9db0922bade40695
anderson <andersongil.dev@gmail.com>
2020-12-10 03:47:05 +0100

    Agregado carpeta para estilos dentro de componentes y containers por separado,cambiando estructura BEM del heading

--- src/resume/components/Heading.jsx
+++ src/resume/components/Heading.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import '../styles/Heading.scss';
+import './styles/Heading.scss';
 
 export const Heading = () => (
     <div className = "Heading">
-        <h2 className = "Heading__title">Resumen de Anderson</h2>
-        <p className = "Heading__p"> Mejorar mis habilidades de Desarrollo a través de la experiencia laboral en equipo.</p>
+        <section>
+
+            <div className = "encabezado">
+                <h2 className = "encabezado__title" > Resumen de Anderson </h2>
+            </div>
+            
+            <p className = "Heading__p"> Mejorar mis habilidades de Desarrollo a través de la experiencia laboral en equipo.</p>
+        </section>
     </div>
 );
 
