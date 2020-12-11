e18b8482abf59e0f3d8efc8a8743472da33366eb
anderson <andersongil.dev@gmail.com>
2020-12-11 02:28:21 +0100

    Mejoras de optimizacion en la carga inicial de la pagina

--- src/shared/routes/App.js
+++ src/shared/routes/App.js
@@ -2,12 +2,14 @@ import React, {Suspense } from 'react';
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import './App.scss';
 import 'materialize-css/dist/css/materialize.min.css';
-import NotFound from '../../notfound/NotFount';
 import Preloader from '../components/Preloader_line';
 //import Header from '../Header';
 //import Layout from '../Layout';
 
 //Lazy components
+const NotFoundPromise =  import('../../notfound/NotFount');
+//esto lo que hace es que notFound se cargue en la primera carga PERO no impida el flujo del render es decir carga en paralelo a la vista actual
+const NotFound = React.lazy( /* webpackChunkName: '7.js' */()=> NotFoundPromise );
 const Layout = React.lazy( () => import ('../Layout') );
 const Resume = React.lazy( () => import ('../../resume') );
 const Home = React.lazy( () => import ('../../home/Home') ); //usando code splitting con rutas
