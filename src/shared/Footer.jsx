81c8cf30ba1b89c718867c23230a6123ecaf34d8
anderson <andersongil.dev@gmail.com>
2021-01-07 12:51:20 +0100

    IMP:footer y layout con indetacion del linter. agregando height minimo en el layout para evitar desplazamiento y mejorar SEO

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -7,74 +7,73 @@ import gitlabPic from './Statics/gitlab.png';
 import correoIcon from './Statics/correo.png';
 import clickUX from './Statics/actionClick.png';
 
-
 const Footer = () =>(
-    <footer className="footer--card">
-        <div className="row">
-            <div className="col l12 offset-s4 s6 footer__contact">
-                <div className = "footer__contact_h5">
-                    
-                    <h5 className="white-text">Contacto</h5>
-                    <hr className="hr--reveal" />
-                </div>                
+  <footer className="footer--card">
+    <div className="row">
+      <div className="col l12 offset-s4 s6 footer__contact">
+        <div className = "footer__contact_h5">
 
-            </div>
-
-            <div className="col l6  offset-s4 s6">
-                <a 
-                    className="grey-text text-lighten-4"
-                    href="mailto:andersongil.dev@gmail.com?Subject=(portafolio)%20Quiero%20hacer%20un%20proyecto%20contigo"
-                >
-                    ¿Quieres que trabajemos juntos en un proyecto?<span className ="hide-on-small-only"> <img src={correoIcon} alt=""/> </span>
-                </a>
-            </div>
-            
-            <div className="col l6 offset-m3 offset-s4 s6">
-                <div className="col s12 offset-m3 m9">
-                    <h5 className="white-text">Sigueme! 
-                        <span>
-                            <img
-                                className="footer__img footer__img--zoom" 
-                                src={clickUX} 
-                                alt="icono de un raton" 
-                                title="Imagen de raton para mejorar UX" 
-                            />
-                        </span>
-                    </h5>
-                    
-                </div>
-                <div className="col s12 m3">
-                    <img src={facebookPic} className="footer__img" width="50px" title="icono de facebook para contacto" alt="contacto por medio de facebook" />
-
-                </div>
-                <div className="col s12 m3">
-                <img src={gitlabPic} className="footer__img" width="50px" title="icono de gitlab para contacto" alt="contacto por medio de gitlab" />
-
-                </div>
-                <div className="col s12 m3">
-                <img src={githubPic} className="footer__img" width="50px" title="icono de github para contacto" alt="contacto por medio de github" />
-
-                </div>
-
-                <div className="col s12 m3">
-                    <img src={linkedinPic} className="footer__img" width="50px" title="icono de linkedin para contacto" alt="contacto por medio de github" />
-
-                </div>
-
-            </div>
-
-        </div>
-        
-        <div className="container">
-            <div className="row">
-
-                <div className="footer-copyright">            
-                    <span className= "white-text right" >&copy; 2020 Anderson Gil. Todos los derechos reservados.</span>
-                </div>
-            </div>
+          <h5 className="white-text">Contacto</h5>
+          <hr className="hr--reveal" />
         </div>
 
-    </footer>
+      </div>
+
+      <div className="col l6  offset-s4 s6">
+        <a
+          className="grey-text text-lighten-4"
+          href="mailto:andersongil.dev@gmail.com?Subject=(portafolio)%20Quiero%20hacer%20un%20proyecto%20contigo"
+        >
+            ¿Quieres que trabajemos juntos en un proyecto?<span className ="hide-on-small-only"> <img src={correoIcon} alt=""/> </span>
+        </a>
+      </div>
+
+      <div className="col l6 offset-m3 offset-s4 s6">
+        <div className="col s12 offset-m3 m9">
+          <h5 className="white-text">Sigueme!
+            <span>
+              <img
+                className="footer__img footer__img--zoom"
+                src={clickUX}
+                alt="icono de un raton"
+                title="Imagen de raton para mejorar UX"
+              />
+            </span>
+          </h5>
+
+        </div>
+        <div className="col s12 m3">
+          <img loading="lazy" src={facebookPic} className="footer__img" width="50px" title="icono de facebook para contacto" alt="contacto por medio de facebook" />
+
+        </div>
+        <div className="col s12 m3">
+          <img src={gitlabPic} className="footer__img" width="50px" title="icono de gitlab para contacto" alt="contacto por medio de gitlab" />
+
+        </div>
+        <div className="col s12 m3">
+          <img src={githubPic} className="footer__img" width="50px" title="icono de github para contacto" alt="contacto por medio de github" />
+
+        </div>
+
+        <div className="col s12 m3">
+          <img src={linkedinPic} className="footer__img" width="50px" title="icono de linkedin para contacto" alt="contacto por medio de github" />
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div className="container">
+      <div className="row">
+
+        <div className="footer-copyright">
+          <span className= "white-text right" >&copy; 2020 Anderson Gil. Todos los derechos reservados.</span>
+        </div>
+      </div>
+    </div>
+
+  </footer>
 );
 
 export default Footer;
--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -4,37 +4,35 @@ import Footer from './Footer'
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleMenu } from './redux';
 
-const Layout = ( {children} ) => {
-    const isOpen = useSelector( state => state.open )
-    const dispatch = useDispatch()
-    const [menu, setMenu] = useState()
-    useEffect(() => {
-        var elem = document.querySelector(".sidenav");
-        var elem2 = document.querySelector(".tooltipped");
-        
-        import ('materialize-css/dist/js/materialize.min.js').then(M => {
-          const side= M.Sidenav.init(elem, {
-              Edge: "left",
-              inDuration: 250
-          });
-          setMenu(side)
-          M.Tooltip.init(elem2,{
-            transitionMovement:20,
-    
-          });
-        })
-      },[]);
+const Layout = ( { children } ) => {
+  const isOpen = useSelector( state => state.open )
+  const dispatch = useDispatch()
+  const [menu, setMenu] = useState()
+  useEffect( () => {
+    var elem = document.querySelector( ".sidenav" );
+    var elem2 = document.querySelector( ".tooltipped" );
+    import ( 'materialize-css/dist/js/materialize.min.js' ).then( M => {
+      const side= M.Sidenav.init( elem, {
+        Edge: "left",
+        inDuration: 250
+      } );
+      setMenu( side )
+      M.Tooltip.init( elem2,{
+        transitionMovement:20,
 
-      useEffect( ()=>{
-        console.log(menu)
-      },[isOpen])
-      return (
-        <>
-            <Header/>
-            {children}
-            <Footer/>
-        </>
-      )
+      } );
+
+    } )
+  },[] );
+  return (
+    <>
+      <Header/>
+      <div style ={{ minHeight: "100vh" }}>
+        {children}
+      </div>
+      <Footer/>
+    </>
+  )
 };
 
 export default Layout;
