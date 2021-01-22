59b6a772714fa03db7f3c660c80e6d8015e0e523
anderson <andersongil.dev@gmail.com>
2021-01-22 18:02:45 +0000

    Updates

---
+++ precache-manifest.5c6f11153a1f950c4f266400aa9a4a86.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569e6eb806b234ef757642793f4846bf",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "3954ba7e185aa9f24db6",
+    "url": "/portafolio/static/css/8.7d838e97.chunk.css"
+  },
+  {
+    "revision": "2882e0451ba66be9768e",
+    "url": "/portafolio/static/css/HomeLazy.d7993c47.chunk.css"
+  },
+  {
+    "revision": "790e02b6160d2d9b9f42",
+    "url": "/portafolio/static/css/LayoutLazy.34e513b1.chunk.css"
+  },
+  {
+    "revision": "db6a0d0a9e6615e2ce65",
+    "url": "/portafolio/static/css/ResumeLazy.e96fd11a.chunk.css"
+  },
+  {
+    "revision": "7c346aa374502a3007cb",
+    "url": "/portafolio/static/css/WorkLazy.d92ac943.chunk.css"
+  },
+  {
+    "revision": "82249e6f8e34f2af281d",
+    "url": "/portafolio/static/css/main.b9d9fb56.chunk.css"
+  },
+  {
+    "revision": "57b4fa9681eed941e04f",
+    "url": "/portafolio/static/js/0.88cf6ee1.chunk.js"
+  },
+  {
+    "revision": "5a67c4cf49105bb22406",
+    "url": "/portafolio/static/js/10.3b55a423.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/portafolio/static/js/10.3b55a423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518ce518ac598bcb9a61",
+    "url": "/portafolio/static/js/11.897d0e4c.chunk.js"
+  },
+  {
+    "revision": "772fd68283dbcd329284",
+    "url": "/portafolio/static/js/12.579abce5.chunk.js"
+  },
+  {
+    "revision": "601c83c85129f3955a169de052f2ac58",
+    "url": "/portafolio/static/js/12.579abce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3954ba7e185aa9f24db6",
+    "url": "/portafolio/static/js/8.19a91b37.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portafolio/static/js/8.19a91b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee63a73b0171d84d504f",
+    "url": "/portafolio/static/js/9.da35824c.chunk.js"
+  },
+  {
+    "revision": "2882e0451ba66be9768e",
+    "url": "/portafolio/static/js/HomeLazy.56f98a9c.chunk.js"
+  },
+  {
+    "revision": "790e02b6160d2d9b9f42",
+    "url": "/portafolio/static/js/LayoutLazy.c4cc07f5.chunk.js"
+  },
+  {
+    "revision": "db6a0d0a9e6615e2ce65",
+    "url": "/portafolio/static/js/ResumeLazy.82de33e4.chunk.js"
+  },
+  {
+    "revision": "7c346aa374502a3007cb",
+    "url": "/portafolio/static/js/WorkLazy.69ce465f.chunk.js"
+  },
+  {
+    "revision": "82249e6f8e34f2af281d",
+    "url": "/portafolio/static/js/main.d3af7ea9.chunk.js"
+  },
+  {
+    "revision": "699125458d6f53e582bd",
+    "url": "/portafolio/static/js/notFoundLazy.a4766549.chunk.js"
+  },
+  {
+    "revision": "1c689bb1808a00d3bb1c",
+    "url": "/portafolio/static/js/runtime-main.98635f31.js"
+  },
+  {
+    "revision": "1d754aeb103e86fdd991e9e575a45b77",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_1_Marcado.1d754aeb.png"
+  },
+  {
+    "revision": "d07edc55fd7620cda1608aaa9a27ea32",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_2_Marcado.d07edc55.png"
+  },
+  {
+    "revision": "7507affb24fc792e4847edd34e03b2de",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_3_Marcado.7507affb.png"
+  },
+  {
+    "revision": "50711ff13ad51f15e5dedbd62286879f",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_4_Marcado.50711ff1.png"
+  },
+  {
+    "revision": "4c3b6b3e01f3789811b0255ee8ce9951",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_5_Marcado.4c3b6b3e.png"
+  },
+  {
+    "revision": "cf3e20655e7b1d524d8cb550e711261d",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_6_Marcado.cf3e2065.png"
+  },
+  {
+    "revision": "9cbbaf4330687a670228ecb5b567e5f0",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_7_Marcado.9cbbaf43.png"
+  },
+  {
+    "revision": "cded3cedd35456c54af73de59c2d3fe8",
+    "url": "/portafolio/static/media/caprofula_edicion_informacion.cded3ced.jpg"
+  },
+  {
+    "revision": "4b8e0140eaeef8186477142e5926f29c",
+    "url": "/portafolio/static/media/caprofula_noticias_ejecucion.4b8e0140.png"
+  },
+  {
+    "revision": "6e66bbc6ddeb2ebc7cea3d1eefa5b81e",
+    "url": "/portafolio/static/media/facebook.6e66bbc6.png"
+  },
+  {
+    "revision": "09ee16c708c0f2fbe4dc72c3749412f9",
+    "url": "/portafolio/static/media/frontal-min.09ee16c7.png"
+  },
+  {
+    "revision": "3eaa7bd5364b15c282bea8548acfa88d",
+    "url": "/portafolio/static/media/github.3eaa7bd5.png"
+  },
+  {
+    "revision": "d3b5ea5c063078cc4a52f12b76573907",
+    "url": "/portafolio/static/media/hero_test.d3b5ea5c.svg"
+  },
+  {
+    "revision": "b0558345f304f3f69acd6a4e08143511",
+    "url": "/portafolio/static/media/proyecto_FoundussApp.b0558345.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portafolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portafolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portafolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portafolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./portafolio/precache-manifest.e250c34985d22753e953b2697d76bdd3.js"
+  "/portafolio/precache-manifest.5c6f11153a1f950c4f266400aa9a4a86.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./portafolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portafolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
