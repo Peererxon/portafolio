1d51dacc97a42c8ab54ef92f19a1dc4c5310623e
anderson <andersongil.dev@gmail.com>
2021-02-08 18:06:14 +0000

    Updates

---
+++ precache-manifest.4976440f1d5a5a63c396662c12dad8a1.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d311af99efe51b27e0267eba38f9b0b2",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "b7ca72c0d2ac68b8590f",
+    "url": "/portafolio/static/css/8.7d838e97.chunk.css"
+  },
+  {
+    "revision": "4a4473c934d8565c2605",
+    "url": "/portafolio/static/css/HomeLazy.eb46b33b.chunk.css"
+  },
+  {
+    "revision": "9328e0472b380ffaaf48",
+    "url": "/portafolio/static/css/LayoutLazy.367e9111.chunk.css"
+  },
+  {
+    "revision": "ee6fb937fa955d103a45",
+    "url": "/portafolio/static/css/ResumeLazy.37656075.chunk.css"
+  },
+  {
+    "revision": "d0803354636ba3b4040b",
+    "url": "/portafolio/static/css/WorkLazy.8494d34b.chunk.css"
+  },
+  {
+    "revision": "dd9fc43781530b4a98ee",
+    "url": "/portafolio/static/css/main.b3a3f06f.chunk.css"
+  },
+  {
+    "revision": "020eeba47601a65247a8",
+    "url": "/portafolio/static/js/0.ef85059d.chunk.js"
+  },
+  {
+    "revision": "e62ad2085950c82253df",
+    "url": "/portafolio/static/js/10.5b685f69.chunk.js"
+  },
+  {
+    "revision": "5473d58a7c3c49a3a773",
+    "url": "/portafolio/static/js/11.0e8e65a1.chunk.js"
+  },
+  {
+    "revision": "601c83c85129f3955a169de052f2ac58",
+    "url": "/portafolio/static/js/11.0e8e65a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ca72c0d2ac68b8590f",
+    "url": "/portafolio/static/js/8.6ca6bd0d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portafolio/static/js/8.6ca6bd0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15f897c0b3d4d06b142",
+    "url": "/portafolio/static/js/9.3e25d43a.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/portafolio/static/js/9.3e25d43a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4473c934d8565c2605",
+    "url": "/portafolio/static/js/HomeLazy.47d154e0.chunk.js"
+  },
+  {
+    "revision": "9328e0472b380ffaaf48",
+    "url": "/portafolio/static/js/LayoutLazy.2e8a419b.chunk.js"
+  },
+  {
+    "revision": "ee6fb937fa955d103a45",
+    "url": "/portafolio/static/js/ResumeLazy.9014abdc.chunk.js"
+  },
+  {
+    "revision": "d0803354636ba3b4040b",
+    "url": "/portafolio/static/js/WorkLazy.df1631f8.chunk.js"
+  },
+  {
+    "revision": "dd9fc43781530b4a98ee",
+    "url": "/portafolio/static/js/main.596449ec.chunk.js"
+  },
+  {
+    "revision": "e4187558055d4f276256",
+    "url": "/portafolio/static/js/notFoundLazy.75447828.chunk.js"
+  },
+  {
+    "revision": "11127e1183f6437f306f",
+    "url": "/portafolio/static/js/runtime-main.88487088.js"
+  },
+  {
+    "revision": "1d754aeb103e86fdd991e9e575a45b77",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_1_Marcado.1d754aeb.png"
+  },
+  {
+    "revision": "d07edc55fd7620cda1608aaa9a27ea32",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_2_Marcado.d07edc55.png"
+  },
+  {
+    "revision": "7507affb24fc792e4847edd34e03b2de",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_3_Marcado.7507affb.png"
+  },
+  {
+    "revision": "50711ff13ad51f15e5dedbd62286879f",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_4_Marcado.50711ff1.png"
+  },
+  {
+    "revision": "4c3b6b3e01f3789811b0255ee8ce9951",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_5_Marcado.4c3b6b3e.png"
+  },
+  {
+    "revision": "cf3e20655e7b1d524d8cb550e711261d",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_6_Marcado.cf3e2065.png"
+  },
+  {
+    "revision": "9cbbaf4330687a670228ecb5b567e5f0",
+    "url": "/portafolio/static/media/Ionic_proyectoCAD_7_Marcado.9cbbaf43.png"
+  },
+  {
+    "revision": "cded3cedd35456c54af73de59c2d3fe8",
+    "url": "/portafolio/static/media/caprofula_edicion_informacion.cded3ced.jpg"
+  },
+  {
+    "revision": "4b8e0140eaeef8186477142e5926f29c",
+    "url": "/portafolio/static/media/caprofula_noticias_ejecucion.4b8e0140.png"
+  },
+  {
+    "revision": "d08b853fcb19dd6d9d0aa034411bf37d",
+    "url": "/portafolio/static/media/eccomerce_develop.d08b853f.jpg"
+  },
+  {
+    "revision": "6e66bbc6ddeb2ebc7cea3d1eefa5b81e",
+    "url": "/portafolio/static/media/facebook.6e66bbc6.png"
+  },
+  {
+    "revision": "09ee16c708c0f2fbe4dc72c3749412f9",
+    "url": "/portafolio/static/media/frontal-min.09ee16c7.png"
+  },
+  {
+    "revision": "3eaa7bd5364b15c282bea8548acfa88d",
+    "url": "/portafolio/static/media/github.3eaa7bd5.png"
+  },
+  {
+    "revision": "d3b5ea5c063078cc4a52f12b76573907",
+    "url": "/portafolio/static/media/hero_test.d3b5ea5c.svg"
+  },
+  {
+    "revision": "c97246fd44e8e2639751bd2a92a078b7",
+    "url": "/portafolio/static/media/mobile_develop.c97246fd.jpg"
+  },
+  {
+    "revision": "b0558345f304f3f69acd6a4e08143511",
+    "url": "/portafolio/static/media/proyecto_FoundussApp.b0558345.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portafolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portafolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portafolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portafolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9f0e39d2df44d60a13407e3608ab5594",
+    "url": "/portafolio/static/media/website_develop.9f0e39d2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.07aadf7763287d012584d1d565778c29.js"
+  "/portafolio/precache-manifest.4976440f1d5a5a63c396662c12dad8a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portafolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
