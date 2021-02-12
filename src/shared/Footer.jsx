f52bc684faf9a4ef689017f3cfdb5dc712935357
anderson <andersongil.dev@gmail.com>
2021-02-12 20:47:18 +0100

    IMP:render dinamico de footer con animaciones distintas para desktop y mobile en las redes y en el copy

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -6,8 +6,9 @@ import githubPic from './Statics/github.png';
 import gitlabPic from './Statics/gitlab.png';
 import correoIcon from './Statics/correo.png';
 import clickUX from './Statics/actionClick.png';
+import { Slide, Roll, Bounce } from "react-awesome-reveal";
 import { useTranslation } from 'react-i18next';
-
+import { screenWidth } from './utils/js';
 // eslint-disable-next-line brace-style
 const Footer = () => {
   const { t } = useTranslation();
@@ -35,7 +36,7 @@ const Footer = () => {
 
         {/* SOCIALSNETWORKS */}
         <div className="col l6 offset-s3 s6 center-align">
-
+          {/* TITLE */}
           <div className="col col--no-padding s12 ">
             <h5 className="white-text">
               { t( "layout:footer:sigueme" ) }
@@ -49,56 +50,110 @@ const Footer = () => {
               </div>
             </h5>
           </div>
+          {/* ENDTITLE */}
 
-          <div className="col col--no-padding s12 m3">
-            <a href="https://www.facebook.com/peererxon" rel="noopener noreferrer" target="_blank" >
-              <img loading="lazy"
-                src={facebookPic}
-                className="footer__img"
-                width="50px"
-                title="imagen de facebook como medio de contacto"
-                alt="facebook de anderson gil para contactarlo"
-              />
-            </a>
-          </div>
-          <div className="col col--no-padding s12 m3">
-            <a href="https://gitlab.com/Peererxon" rel="noopener noreferrer" target="_blank">
-              <img src={gitlabPic} 
-                className="footer__img"
-                width="50px"
-                title="imagen de gitlab como medio de contacto"
-                alt="gitlab de anderson gil para contactarlo"
-              />
+          {/* NETWORKS */}
+          { screenWidth < 600 ?
+            <Roll cascade triggerOnce={true}>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://www.facebook.com/peererxon" rel="noopener noreferrer" target="_blank" >
+                  <img loading="lazy"
+                    src={facebookPic}
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de facebook como medio de contacto"
+                    alt="facebook de anderson gil para contactarlo"
+                  />
+                </a>
+              </div>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://gitlab.com/Peererxon" rel="noopener noreferrer" target="_blank">
+                  <img src={gitlabPic} 
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de gitlab como medio de contacto"
+                    alt="gitlab de anderson gil para contactarlo"
+                  />
 
-            </a>
+                </a>
 
-          </div>
-          <div className="col col--no-padding s12 m3">
-            <a href="https://github.com/Peererxon" rel="noopener noreferrer" target="_blank">
-              <img src={githubPic} 
-                className="footer__img"
-                width="50px"
-                title="imagen de github como medio de contacto"
-                alt="github de anderson gil para contactarlo"
-              />
-            </a>
-          </div>
-          <div className="col col--no-padding s12 m3">
-            <a href="https://ve.linkedin.com/in/anderson-gil-petit-56a923180" rel="noopener noreferrer" target="_blank">
-              <img src={linkedinPic} 
-                className="footer__img"
-                width="50px"
-                title="imagen de linkedin como medio de contacto"
-                alt="linkedin de anderson gil para contactarlo"
-              />
-            </a>
-          </div>
+              </div>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://github.com/Peererxon" rel="noopener noreferrer" target="_blank">
+                  <img src={githubPic} 
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de github como medio de contacto"
+                    alt="github de anderson gil para contactarlo"
+                  />
+                </a>
+              </div>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://ve.linkedin.com/in/anderson-gil-petit-56a923180" rel="noopener noreferrer" target="_blank">
+                  <img src={linkedinPic} 
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de linkedin como medio de contacto"
+                    alt="linkedin de anderson gil para contactarlo"
+                  />
+                </a>
+              </div>
+            </Roll> :
+            <Bounce cascade triggerOnce={true}>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://www.facebook.com/peererxon" rel="noopener noreferrer" target="_blank" >
+                  <img loading="lazy"
+                    src={facebookPic}
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de facebook como medio de contacto"
+                    alt="facebook de anderson gil para contactarlo"
+                  />
+                </a>
+              </div>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://gitlab.com/Peererxon" rel="noopener noreferrer" target="_blank">
+                  <img src={gitlabPic} 
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de gitlab como medio de contacto"
+                    alt="gitlab de anderson gil para contactarlo"
+                  />
+
+                </a>
+
+              </div>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://github.com/Peererxon" rel="noopener noreferrer" target="_blank">
+                  <img src={githubPic} 
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de github como medio de contacto"
+                    alt="github de anderson gil para contactarlo"
+                  />
+                </a>
+              </div>
+              <div className="col col--no-padding s12 m3">
+                <a href="https://ve.linkedin.com/in/anderson-gil-petit-56a923180" rel="noopener noreferrer" target="_blank">
+                  <img src={linkedinPic} 
+                    className="footer__img"
+                    width="50px"
+                    title="imagen de linkedin como medio de contacto"
+                    alt="linkedin de anderson gil para contactarlo"
+                  />
+                </a>
+              </div>
+            </Bounce>
+          }
+          {/* ENDNETWORKS */}
         </div>
         {/* ENDSOCIALSNETWORKS */}
         {/* COPYRIGHT */}
         <div className="col col--no-padding s6 offset-s3 m4 offset-m4">
           <div className="footer-copyright center-align">
-            <span className= "white-text" >&copy; 2020 Anderson Gil. { t( "layout:footer:derechos" ) }</span>
+            <Slide direction="up">
+              <span className= "white-text" >&copy; 2020 Anderson Gil. { t( "layout:footer:derechos" ) }</span>
+            </Slide>
           </div>
         </div>
         {/* ENDCOPYRIGHT */}
