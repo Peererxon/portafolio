985663287db15ae66b75e52d5d7de4fbb1e42378
anderson <andersongil.dev@gmail.com>
2021-02-16 16:34:06 +0100

    WIP: Eliminando memo dondela aplicacion no lo utiliza, centrando titulos en el resumen y dando fuente oscura a los mismos. exportando componentes por el nombre de su constante en lugar de default para mejorar el uso de las react developers tools y ver el rendimiento a detalle

--- src/resume/components/SectionBody.jsx
+++ src/resume/components/SectionBody.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
-
+import './styles/SectionBody.scss';
 import PropTypes from 'prop-types'
-
-const SectionBody = ( { elemento } ) => {
+export const SectionBody = ( { elemento } ) =>
+{
   return (
     <div className="sectionbody">
       <ul>
-        {elemento.titulos.map( ( titulo, indice ) => <li style = {{ margin: '10px 0px' }} key= {indice}> {titulo}</li> )}
-        <li>
+        {
+          elemento.titulos.map(
+            ( titulo, indice ) =>
+            {
+              if ( indice === 0 )
+              {
+                return <li key= {indice} className=" sectionbody__li sectionbody__li--withTitle"> {titulo}</li>;
+              }
+              return <li key= {indice} className="sectionbody__li" > {titulo} </li>;
+            }
+          )
+        }{/* END map function */}
+
+        <li className="sectionbody__li">
           {elemento.contenido}
 
         </li>
       </ul>
     </div>
-
   )
 }
 
 SectionBody.propTypes = {
   elemento: PropTypes.shape( { titulos: PropTypes.array, contenido: PropTypes.string } )
 }
-
-export default SectionBody;
