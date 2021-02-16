4b47b6c6ef0f56f6a4f1c5c990160b5a072aff38
anderson <andersongil.dev@gmail.com>
2021-02-16 19:03:07 +0100

    IMP: libreria para utilizar el lenguaje del navegador, utilizandola en la aplicación

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './shared/utils/i18n/en/index.json';
 import es from './shared/utils/i18n/es/index.json'
 
 i18n
-  .use(initReactI18next)
+  .use( LanguageDetector )
+  .use( initReactI18next )
   .init({
-    resources: {en,es},
-    fallbackLng: 'es'
-  });
+    resources: { en, es },
+    fallbackLng: 'es',
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  } );
+i18n.changeLanguage()
 
 export default i18n;
